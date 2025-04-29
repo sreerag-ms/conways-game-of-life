e2b8137dc5d946938aee12eb6567886b2d5de252
Sreerag M S <sreeragmsadanandan@gmail.com>
2025-04-29 20:45:53 +0100

    Updated the prod path to repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),    react()],
+  plugins: [tailwindcss(), react()],
+  base: process.env.NODE_ENV === 'production' ? '/conways-game-of-life/' : '/',
   css: {
     postcss: './postcss.config.js',
   }
